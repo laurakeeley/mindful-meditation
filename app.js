b8fda2b040c0f1aa3f47d699a62e32bd10b85bb2
laurakeeley <l.keeley000@gmail.com>
2023-04-23 11:51:59 -0500

    adds js to change time, play pause song and video and change option

--- app.js
+++ app.js
@@ -8,18 +8,36 @@ const app = () => {
     const sounds = document.querySelectorAll('.sound-picker button');
     //time display
     const timeDisplay = document.querySelector('.time-display');
-    const timeSelect = document.querySelector('.time button')
+    const timeSelect = document.querySelectorAll('.time button');
     //get length of the outline
     const outlineLength = outline.getTotalLength();
     //duration
-    let maxDuration = 300; //default
+    let duration = 300; //default
 
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
+    //select sound
+    sounds.forEach(soundOption => {
+        soundOption.addEventListener('click', function () {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        })
+    })
+    
     //play song
     play.addEventListener('click', () => {
         checkPlaying(song);
+    });
+
+    //select time
+    timeSelect.forEach(timeOption => {
+        timeOption.addEventListener('click', function () {
+            duration = this.getAttribute('data-time');
+            console.log(duration);
+            timeDisplay.textContent = `${Math.floor(duration / 60)}:${Math.floor(duration % 60)}`;
+        })
     });
 
     //function to stop and play sound
@@ -38,14 +56,22 @@ const app = () => {
     //animate timer
     song.ontimeupdate = function () {
         let currentTime = song.currentTime;
-        let elapsed = maxDuration - currentTime;
+        let elapsed = duration - currentTime;
         let seconds = Math.floor(elapsed % 60);
         let mintues = Math.floor(elapsed / 60);
         //animate circle
-        let progress = outlineLength - (currentTime / maxDuration) * outlineLength;
+        let progress = outlineLength - (currentTime / duration) * outlineLength;
         outline.style.strokeDashoffset = progress;
         //animate time
         timeDisplay.textContent = `${mintues}:${seconds}`;
+
+        //stop at 0
+        if (currentTime >= duration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
     }
 }
 
